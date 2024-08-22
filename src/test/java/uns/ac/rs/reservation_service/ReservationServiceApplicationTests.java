6369861c2341520a3a9ab4fd668d7b1afb716b32
Nikola Jovic <jovnikola99@gmail.com>
2024-08-22 23:04:12 +0200

    Develop (#4)
    * feat: Adding initial Spring Boot project
    
    * feat: Adding CICD pipeline
    
    * feat: Adding Docker configuration (#3)

---
+++ src/test/java/uns/ac/rs/reservation_service/ReservationServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package uns.ac.rs.reservation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
